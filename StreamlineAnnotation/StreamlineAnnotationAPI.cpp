b1b385b0d8171cf92bc83adab9881213566e56c3
Gabor Rapcsanyi <rgabor@inf.u-szeged.hu>
2013-10-14 15:03:08 +0200

    Refactoring startColorizedAnnotate() method

--- StreamlineAnnotation/StreamlineAnnotationAPI.cpp
+++ StreamlineAnnotation/StreamlineAnnotationAPI.cpp
@@ -37,28 +37,34 @@ void StreamlineAnnotationAPI::startAnnotate(const std::string& text)
 
 void StreamlineAnnotationAPI::startColorizedAnnotate(const std::string& color, const std::string& text)
 {
+    char* str = const_cast<char*>(text.c_str());
+    int annotation_color = 0;
+
     if (color.compare("red") == 0 || color.compare("RED") == 0)
-        ANNOTATE_COLOR(ANNOTATE_RED, const_cast<char*>(text.c_str()));
+        annotation_color = ANNOTATE_RED;
     else if (color.compare("blue") == 0 || color.compare("BLUE") == 0)
-        ANNOTATE_COLOR(ANNOTATE_BLUE, const_cast<char*>(text.c_str()));
+        annotation_color = ANNOTATE_BLUE;
     else if (color.compare("green") == 0 || color.compare("GREEN") == 0)
-        ANNOTATE_COLOR(ANNOTATE_GREEN, const_cast<char*>(text.c_str()));
+        annotation_color = ANNOTATE_GREEN;
     else if (color.compare("purple") == 0 || color.compare("PURPLE") == 0)
-        ANNOTATE_COLOR(ANNOTATE_PURPLE, const_cast<char*>(text.c_str()));
+        annotation_color = ANNOTATE_PURPLE;
     else if (color.compare("yellow") == 0 || color.compare("YELLOW") == 0)
-        ANNOTATE_COLOR(ANNOTATE_YELLOW, const_cast<char*>(text.c_str()));
+        annotation_color = ANNOTATE_YELLOW;
     else if (color.compare("cyan") == 0 || color.compare("CYAN") == 0)
-        ANNOTATE_COLOR(ANNOTATE_CYAN, const_cast<char*>(text.c_str()));
+        annotation_color = ANNOTATE_CYAN;
     else if (color.compare("white") == 0 || color.compare("WHITE") == 0)
-        ANNOTATE_COLOR(ANNOTATE_WHITE, const_cast<char*>(text.c_str()));
+        annotation_color = ANNOTATE_WHITE;
     else if (color.compare("ltgray") == 0 || color.compare("LTGRAY") == 0)
-        ANNOTATE_COLOR(ANNOTATE_LTGRAY, const_cast<char*>(text.c_str()));
+        annotation_color = ANNOTATE_LTGRAY;
     else if (color.compare("dkgray") == 0 || color.compare("DKGRAY") == 0)
-        ANNOTATE_COLOR(ANNOTATE_DKGRAY, const_cast<char*>(text.c_str()));
+        annotation_color = ANNOTATE_DKGRAY;
     else if (color.compare("black") == 0 || color.compare("BLACK") == 0)
-        ANNOTATE_COLOR(ANNOTATE_BLACK, const_cast<char*>(text.c_str()));
+        annotation_color = ANNOTATE_BLACK;
+
+    if (!annotation_color)
+        ANNOTATE(str);
     else
-        ANNOTATE(const_cast<char*>(text.c_str()));
+        ANNOTATE_COLOR(annotation_color, str);
 }
 
 void StreamlineAnnotationAPI::stopAnnotate()
